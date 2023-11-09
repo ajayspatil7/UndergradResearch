20071c3b7457a6da321518b8a00ff0fb1e95cf14
ajayspatil7 <aspatil99451@gmail.com>
2023-11-09 14:14:27 +0530

    Implemented Naive base algorithm

---
+++ naivebayes/naive.py
@@ -0,0 +1,55 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class NaiveBayes:
+
+    def __init__(self):
+        self.priors = None
+        self.var = None
+        self.mean = None
+        self.classes = None
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self.classes = np.unique(y)
+        n_classes = len(self.classes)
+        # calculate mean, var, and prior for each class
+        self.mean = np.zeros((n_classes, n_features), dtype=np.float64)
+        self.var = np.zeros((n_classes, n_features), dtype=np.float64)
+        self.priors = np.zeros(n_classes, dtype=np.float64)
+
+        for idx, c in enumerate(self.classes):
+            X_c = X[y == c]
+            self.mean[idx, :] = X_c.mean(axis=0)
+            self.var[idx, :] = X_c.var(axis=0)
+            self.priors[idx] = X_c.shape[0] / float(n_samples)
+
+    def predict(self, X):
+        y_pred = [self._predict(x) for x in X]
+        return np.array(y_pred)
+
+    def _predict(self, x):
+        posteriors = []
+
+        # calculate posterior probability for each class
+        for idx, c in enumerate(self.classes):
+            prior = np.log(self.priors[idx])
+            posterior = np.sum(np.log(self._pdf(idx, x)))
+            posterior = posterior + prior
+            posteriors.append(posterior)
+
+        # return class with the highest posterior
+        return self.classes[np.argmax(posteriors)]
+
+    def _pdf(self, class_idx, x):
+        mean = self.mean[class_idx]
+        var = self.var[class_idx]
+        numerator = np.exp(-((x - mean) ** 2) / (2 * var))
+        denominator = np.sqrt(2 * np.pi * var)
+        return numerator / denominator
+
+    @staticmethod
+    def accuracy(y_test, y_pred):
+        accuracy = np.sum(y_test == y_pred) / len(y_test)
+        return accuracy
